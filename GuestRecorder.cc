a1ded8c65e5eadd55acc4e6251aaca39f489dcf8
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-03-19 13:28:04 -0400

    Add files via upload

--- GuestRecorder.cc
+++ GuestRecorder.cc
@@ -8,9 +8,15 @@ GuestRecorder::GuestRecorder(string n):Recorder(n){
 }
 
 void GuestRecorder::update(Reservation* a){
-	//
+	//if the guest is premium
+	//form a string that is pretty much just guest name and duration of stay using string stream
 	stringstream ss;
-	cout<<"TEST FOR GUEST"<<endl;
+	//cout<<"TEST FOR GUEST"<<endl;
+	if(a->getGuest().getPremium()){
+		//cout<<"IT IS PREMIUM"<<endl;
+		ss<<"-- GUEST NAME:"<<a->getGuest().getName()<<" -> AND YOUR STAY DURATION IS: "<<a->getStay();
+		record.push_back(ss.str());
+	}
 	
 	//a->print();
 }
