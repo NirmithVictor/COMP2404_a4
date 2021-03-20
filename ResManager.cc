111acdbc48431eb34da770a8a26cf99db1b149e9
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-03-20 13:37:41 -0400

    Add files via upload

--- ResManager.cc
+++ ResManager.cc
@@ -5,3 +5,7 @@ using namespace std;
 
 #include "Hotel.h"
 #include "ResManager.h"
+
+ResManager::ResManager(Hotel* a):h(a),res_count(0){
+	cout<<res_count<<endl;s
+}
--- ResManager.h
+++ ResManager.h
@@ -2,15 +2,27 @@
 #define RESMANAGER_H
 
 #include "Recorder.h"
+#include "Reservation.h"
+
+#include <vector>
 
 class Hotel;
 class ResManager
 {
   public:
-
+	ResManager(Hotel*);
+	~ResManager();
+	void setHotel(Hotel*);
+	void addReservation(string, int, int, int, int, ReqRoomType);
+	void subscribe(Recorder*);
+	void print();
+	void printRecords();
+	void notify(Reservation*);
   private:
-  
-
+  	Hotel* h;
+	Reservation* res[MAX_ARR];
+	int res_count;
+	vector<Recorder*> p;
 };
 
 #endif
