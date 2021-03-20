7a42a3c26407ed1692ce51d7983dfd54872d306e
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-03-20 14:43:22 -0400

    Add files via upload

--- ResManager.cc
+++ ResManager.cc
@@ -21,5 +21,14 @@ ResManager::~ResManager(){
 //
 void ResManager::setHotel(Hotel* a){
 	h=a;
-	
 }
+
+//addReservation function breakdown
+/*
+1. stay value must be below 31 if not then break else continue
+2. Dynamically allocate a new Date object with given said date store in a temporary pointer
+3. loop over hotel room criteria
+	3.1 it must satify or exist in the req
+	3.2 check if the room is available for every starting date
+4. if no room is available break	
+*/
